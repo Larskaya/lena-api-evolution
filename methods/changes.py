1f316faac2a83f77e1f8b8c7db11be144f8d1881
Larskaya <llarskaya@ya.ru>
2021-04-17 16:47:13 +0300

    skills

--- methods/changes.py
+++ methods/changes.py
@@ -11,7 +11,7 @@ def connect_db():
     conn = psycopg2.connect(
         database="evolution", 
         user="postgres", 
-        password='user',
+        
         host="localhost", 
         port="5432"
     )
