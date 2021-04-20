e00b6a888b5f674a0e0c517fc942ee333fc186b1
Larskaya <llarskaya@ya.ru>
2021-04-20 22:47:00 +0300

    cookies

--- routs/login.py
+++ routs/login.py
@@ -1,22 +1,29 @@
 from __main__ import app
-#from database.EvolutionDB import EvolDataBase
 import flask
-from flask import request, jsonify
-from werkzeug.security import check_password_hash
-
+from flask import request, jsonify, make_response, render_template
 from App import App
+from werkzeug.security import check_password_hash
 
 @app.route('/login', methods=['POST'])
 def login():
     if request.method == 'POST':
         data = App.login(request.form['login'])
-        print('all data for login', data)
         user_id = data[0]
         user_hpsw = data[1]
 
-        if check_password_hash( user_hpsw, request.form['psw']):
+        if check_password_hash( user_hpsw, request.form['psw'] ):
             code = data[2]
             if code:
-                return jsonify( {"success": True, "code": code, "id": user_id} )
+
+                log = ''
+                if request.cookies.get('code'):
+                    log = request.cookies.get('code')
+
+                res = make_response('authorization: {log}')
+                res.set_cookie('code', str(code))
+                return res
+            
         return jsonify( {"success": False} )
     return render_template('docs.html')
+
+
