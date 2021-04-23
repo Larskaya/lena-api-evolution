f500bdc2805f0c0eee6b5b9bc31df5ed4fdd9f9b
Larskaya <llarskaya@ya.ru>
2021-04-24 02:36:20 +0300

    cookies

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ def connect_db():
         conn = psycopg2.connect(
             database="evolution", 
             user="postgres", 
-            password='user',
+            
             host="localhost", 
             port="5432"
         )
