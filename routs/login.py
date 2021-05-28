45a90b0940733ad24ee7a8fc9a8e7ee1b60edf3a
Larskaya <llarskaya@ya.ru>
2021-05-29 00:07:43 +0300

    login

--- routs/login.py
+++ routs/login.py
@@ -2,31 +2,38 @@ from __main__ import app
 import flask
 from flask import request, jsonify, make_response, render_template
 from App import App
-from werkzeug.security import check_password_hash
+from werkzeug.security import check_password_hash, generate_password_hash
 
 
+# def check_auth_user(login, hpsw):
+#     pass
 @app.route('/auth/login', methods=['POST'])
 def login():
+    #data = App.login(request.form['login'])
+    #print('request login', request.form['login'])
+
     data = App.login(request.form['login'])
+    print('all data for login:', data) # id hpsw code
     user_id = data[0]
     user_hpsw = data[1]
 
-    if check_password_hash( user_hpsw, request.form['psw'] ):
-        code = data[2]
-        if code:
-            log = ''
-            if request.cookies.get('code') and request.cookies.get('user_id'):
-                log = str(request.cookies.get('code')) + str(request.cookies.get('user_id'))
-
-            #res = make_response('authorization: {log}')
-            res = flask.jsonify('authorization: {log}')
-            res.set_cookie('code', str(code))
-            res.set_cookie('user_id', str(user_id))
-            res.headers.add('Set-Cookie','cross-site-cookie=bar; SameSite=None; Secure')
-            res.headers.add('Access-Control-Allow-Origin', '*')
-            return res
-        
-        return jsonify( {"success": False, 'error': 'code'} )
+    if check_password_hash( user_hpsw, request.form['psw'] ): # проверка по паролю 
+        #if App.check_login():
+            code = data[2]
+            if code:
+                # log = ''
+                # if request.cookies.get('code') and request.cookies.get('user_id'):
+                #     log = str(request.cookies.get('code')) + str(request.cookies.get('user_id'))
+                res = flask.jsonify({'success': True})
+                res.set_cookie('code', str(code))
+                res.set_cookie('user_id', str(user_id))
+                res.headers.add('Set-Cookie','cross-site-cookie=bar; SameSite=None; Secure')
+                res.headers.add('Access-Control-Allow-Origin', '*')
+                return res
+            
+            return jsonify( {"success": False, 'error': 'code'} )
+        #else:
+        #    return jsonify( {'success': False, 'error': 'login not correct'} )
     return jsonify( {"success": False, 'error': 'password not correct'} )
 
 
@@ -38,7 +45,6 @@ def logout():
     #res.headers.add('Set-Cookie','cross-site-cookie=bar; SameSite=None; Secure')
     res.headers.add('Access-Control-Allow-Origin', '*')
     return res
-
 
 @app.route('/auth/logout_full', methods=['POST'])
 def logout_full():
